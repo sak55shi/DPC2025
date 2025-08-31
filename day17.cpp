153cc46630e080aed1928dc1089cecfaa6521970
sak55shi <sakshi.wable24@vit.edu>
2025-08-31 10:54:18 +0530

    Update day17.cpp

--- day17.cpp
+++ day17.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
 
+vector<int> primeFactorization(int N) {
+    vector<int> factors;
+    
+    while (N % 2 == 0) {
+        factors.push_back(2);
+        N /= 2;
+    }
+    
+    for (int i = 3; i <= sqrt(N); i += 2) {
+        while (N % i == 0) {
+            factors.push_back(i);
+            N /= i;
+        }
+    }
+   
+    if (N > 1) {
+        factors.push_back(N);
+    }
+    return factors;
+}
+
+int main() {
+    int N;
+    cout << "Enter N: ";
+    cin >> N;
+    vector<int> factors = primeFactorization(N);
+    cout << "[";
+    for (int i = 0; i < factors.size(); i++) {
+        cout << factors[i];
+        if (i < factors.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+    return 0;
+}
